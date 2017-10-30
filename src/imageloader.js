40eb53e014ec757bf80ed6387450763ce0799634
Zword <jesseljohn130@gmail.com>
2017-10-30 15:19:23 +0530

    Update imageloader.js

--- src/imageloader.js
+++ src/imageloader.js
@@ -5,7 +5,7 @@
  * https://github.com/JesselJohn
  * MIT Licence
  */
-! function(window, document, undefined) {
+! function(window, document, angular, undefined) {
     angular.module('ngLazyImg', [])
         .directive('ngLazyLoadImgParent', [function() {
             var ngLazyLoadImgParent = {
@@ -33,12 +33,16 @@
                 var arrOfLazyImgElems = [],
                     animationFrameTimeoutId,
                     timeoutId,
+                    scrollTimeoutId,
                     ngLazyLoadImg = {
                         scope: {
                             'defaultImage': '=?',
                             'imgsrc': '=',
+                            'loadOffsetTop': '=?',
+                            'loadOffsetLeft': '=?',
                             'onWinLoad': '=?',
-                            'isImageLoaded': "=",
+                            'isImageLoaded': "=?",
+                            'imageOnSet': "&",
                             'imageInViewport': "&",
                             'elemWidth': "=?",
                             'elemHeight': "=?"
@@ -48,8 +52,8 @@
                         bindToController: true,
                         template: function(elem, attrs) {
                             return attrs.elemType == 'img' ?
-                                "<img class='lazy-img' ng-src=\"{{tyShow.imgsrc}}\" ng-class=\"{'show':tyShow.loaded}\"></img>" :
-                                "<div class='lazy-img' ng-style=\"{'background-image':'url('+tyShow.imgsrc+')'}\" ng-class=\"{'show':tyShow.loaded}\"></div>";
+                                "<img class='lazy-img' ng-src=\"{{tyShow.loaded && tyShow.imgsrc || tyShow.imgsrcRef}}\" ng-class=\"{'show':tyShow.loaded}\"></img>" :
+                                "<div class='lazy-img' ng-style=\"{'background-image':'url('+(tyShow.loaded && tyShow.imgsrc || tyShow.imgsrcRef)+')'}\" ng-class=\"{'show':tyShow.loaded}\"></div>";
                         },
                         restrict: 'E',
                         controllerAs: 'tyShow',
@@ -57,27 +61,96 @@
                             function($scope, $element) {
                                 var self = this;
 
-                                self.imgsrcRef = self.imgsrc;
-                                self.imgsrc = null;
                                 $scope.self = self;
+                                throwErrorsInPassedValuesFn(
+                                    self.onWinLoad,
+                                    self.preventLazyInview,
+                                    self.loadOffsetTop,
+                                    self.loadOffsetLeft,
+                                    self.elemWidth,
+                                    self.elemHeight
+                                );
                             }
                         ],
                         link: function postLink(scope, element, attrs, ngLazyLoadImgParent) {
                             var item = {
-                                'scope': scope.self,
+                                'scope': scope,
+                                'self': scope.self,
                                 'elem': element
                             };
 
                             scope.self.parentViewport = ngLazyLoadImgParent && ngLazyLoadImgParent.elem ||
                                 null;
 
-                            if (scope.onWinLoad) {
-                                loadItemFn(item, getWindowValuesFn());
+                            scope.$$postDigest(function() {
+                                if (isImageInViewPortFn(item, getWindowValuesFn())) {
+                                    loadItemFn(item);
+                                }
+                            });
+
+                            if (scope.self.onWinLoad) {
+                                // Load Items needed to be loaded initially
+                                loadItemFn(item);
                             } else {
+                                // Push items to be tracked if its in viewport in array
                                 arrOfLazyImgElems.push(item);
+                                // Untrack item if scope destroyed
+                                scope.$on('$destroy', function() {
+                                    removeItemFromArrayFn(arrOfLazyImgElems, item);
+                                });
                             }
                         }
                     };
+
+                function checkBool(val) {
+                    return typeof val === 'boolean' ||
+                        (typeof val === 'object' &&
+                            val !== null &&
+                            typeof val.valueOf() === 'boolean');
+                }
+
+                function checkNum(val) {
+                    return !isNaN(parseInt(val, 10));
+                }
+
+                function throwErrorFn(err) {
+                    throw Error(err);
+                }
+
+                function throwErrorsInPassedValuesFn(
+                    onWinLoad,
+                    preventLazyInview,
+                    loadOffsetTop,
+                    loadOffsetLeft,
+                    elemWidth,
+                    elemHeight
+                ) {
+                    var ERROR_PREFIX = "`ngLazyLoadImg` directive : ";
+                    if (onWinLoad !== undefined && !checkBool(onWinLoad)) {
+                        throwErrorFn(ERROR_PREFIX + "Passed values of loadOffsetTop & loadOffsetLeft should be numbers");
+                    }
+
+                    if (preventLazyInview !== undefined && !checkBool(preventLazyInview)) {
+                        throwErrorFn(ERROR_PREFIX + "Passed values of loadOffsetTop & loadOffsetLeft should be numbers");
+                    }
+
+                    if (loadOffsetTop !== undefined && !checkNum(loadOffsetTop) ||
+                        loadOffsetLeft !== undefined && !checkNum(loadOffsetLeft)) {
+                        throwErrorFn(ERROR_PREFIX + "Passed values of loadOffsetTop & loadOffsetLeft should be numbers");
+                    }
+
+                    if (elemWidth !== undefined && !checkNum(elemWidth) ||
+                        elemHeight !== undefined && !checkNum(elemHeight)) {
+                        throwErrorFn(ERROR_PREFIX + "Passed values of elemWidth & elemHeight should be numbers");
+                    }
+                }
+
+                function removeItemFromArrayFn(arr, item) {
+                    var index = arr.findIndex(i => i === item);
+                    if (index > -1) {
+                        arr.splice(arr.findIndex(i => i === item), 1);
+                    }
+                }
 
                 function getWindowValuesFn() {
                     var doc = document.documentElement;
@@ -116,11 +189,22 @@
                     };
                 }
 
-                function triggerImageInViewportFn(item, windowValues) {
-                    if (isImageInViewPortFn(item, windowValues)) {
-                        item.scope.imageInViewport({
-                            item: item
+                function triggerImageInViewportFn(obj, bypassCondition) {
+                    var isInView = isImageInViewPortFn(obj.item, obj.windowValues);
+                    if (isInView || bypassCondition) {
+                        obj.item.self.imageInViewport({
+                            item: obj.item,
+                            // Boolean value. `true` if image in view
+                            isInView: isInView,
+                            // Boolean value to track image failure. `true` if image load failed
+                            error: obj.error
                         });
+                    }
+                }
+
+                function triggerLocalDigestFn(scope) {
+                    if (!$rootScope.$$phase && !scope.$$phase) {
+                        scope.$digest();
                     }
                 }
 
@@ -131,54 +215,75 @@
                     }, 300, false);
                 }
 
-                function loadItemFn(item, windowValues) {
-                    if (!item.scope.imgsrcRef) {
+                function loadItemFn(item) {
+                    if (!item.self.imgsrc) {
+                        item.self.imgsrcRef = item.self.defaultImage;
+                        item.self.loaded = true;
                         return;
                     }
-                    item.scope.imgsrc = item.scope.imgsrcRef;
-                    item.scope.loaded = true;
-                    if (!item.scope.isImageLoaded) {
+
+                    if (!item.self.loaded) {
+                        item.self.imgsrcRef = item.self.imgsrc;
+                        item.self.loaded = true;
+                        item.self.imageOnSet({
+                            item: item
+                        });
+
                         item.elem[0].onload = function(e) {
-                            item.scope.isImageLoaded = true;
-                            triggerImageInViewportFn(item, windowValues);
+                            item.self.isImageLoaded = true;
+                            item.self.isImageLoadedTracker = true;
+                            item.self.imgError = false;
+                            triggerImageInViewportFn({
+                                item: item,
+                                windowValues: getWindowValuesFn(),
+                                error: false
+                            }, true);
                             triggerGlobalDigestFn();
                         };
                         item.elem[0].onerror = function(e) {
-                            item.scope.isImageLoaded = true;
-                            item.scope.imgsrc = item.scope.defaultImage;
-                            triggerImageInViewportFn(item, windowValues);
+                            item.self.isImageLoaded = true;
+                            item.self.isImageLoadedTracker = true;
+                            item.self.imgError = true;
+                            item.self.imgsrcRef = item.self.defaultImage;
+                            triggerImageInViewportFn({
+                                item: item,
+                                windowValues: getWindowValuesFn(),
+                                error: true
+                            }, true);
                             triggerGlobalDigestFn();
                         };
-                    } else {
-                        triggerImageInViewportFn(item, windowValues);
                     }
-                    triggerGlobalDigestFn();
+
+                    triggerLocalDigestFn(item.scope);
                 }
 
-                function isImageInViewPortFn(item, windowValues) {
+                function isImageInViewPortFn(item, windowValues, loadOffsetTop = 0, loadOffsetLeft = 0) {
                     var box = item.elem[0].getBoundingClientRect();
 
-                    var $elementTop = getCoords(item.elem[0]).top,
-                        $elementLeft = getCoords(item.elem[0]).left;
+                    var loadOffsetTopAbs = Math.abs(parseInt(loadOffsetTop, 10)),
+                        loadOffsetLeftAbs = Math.abs(parseInt(loadOffsetLeft, 10));
 
-                    var passedWidth = item.scope.elemWidth !== undefined && parseInt(item.scope.elemWidth, 10) || 1,
-                        passedHeight = item.scope.elemHeight !== undefined && parseInt(item.scope.elemHeight, 10) || 1,
+                    var $elementTop = getCoords(item.elem[0]).top - loadOffsetTopAbs,
+                        $elementLeft = getCoords(item.elem[0]).left - loadOffsetLeftAbs;
+
+                    var passedWidth = item.self.elemWidth !== undefined && parseInt(item.self.elemWidth, 10) || 1,
+                        passedHeight = item.self.elemHeight !== undefined && parseInt(item.self.elemHeight, 10) || 1,
                         elemHeight = passedWidth !== undefined && passedHeight !== undefined ?
                         box.width * passedHeight / passedWidth :
                         box.height;
 
-                    var $elementBottom = $elementTop + elemHeight,
-                        $elementRight = $elementLeft + box.width;
+                    var $elementBottom = $elementTop + elemHeight + (loadOffsetTopAbs * 2),
+                        $elementRight = $elementLeft + box.width + (loadOffsetLeftAbs * 2);
 
                     var parentViewportBox,
                         $viewportTop, $viewportLeft, $viewportBottom, $viewportRight, $viewportRight,
                         isVisibleInParentViewport = true;
 
-                    if (item.scope.parentViewport !== null) {
-                        parentViewportBox = item.scope.parentViewport[0].getBoundingClientRect();
+                    if (item.self.parentViewport !== null) {
+                        parentViewportBox = item.self.parentViewport[0].getBoundingClientRect();
 
-                        $viewportTop = getCoords(item.scope.parentViewport[0]).top;
-                        $viewportLeft = getCoords(item.scope.parentViewport[0]).left;
+                        $viewportTop = getCoords(item.self.parentViewport[0]).top;
+                        $viewportLeft = getCoords(item.self.parentViewport[0]).left;
                         $viewportBottom = $viewportTop + parentViewportBox.height;
                         $viewportRight = $viewportLeft + parentViewportBox.width;
 
@@ -201,39 +306,50 @@
                             $elementLeft <= windowValues.left && $elementRight >= windowValues.right);
                 }
 
-                function setImageForElemsInArrayFn(arr) {
+                function setImageIfInViewportFn() {
                     var windowValues = getWindowValuesFn(),
-                        i = 0,
-                        reqAnimationFn = function() {
-                            var item = arr[i++];
-                            if (!item) {
-                                return;
-                            }
-                            if (isImageInViewPortFn(item, windowValues)) {
-                                loadItemFn(item, windowValues);
-                            }
-                            animationFrameTimeoutId = $window.requestAnimationFrame(reqAnimationFn);
-                        };
-
-
-                    return reqAnimationFn;
+                        arrOfLoadedElems = [];
+                    arrOfLazyImgElems.forEach(function(item, index) {
+                        var isCompletelyInView = isImageInViewPortFn(item, windowValues);
+                        if ((isImageInViewPortFn(item, windowValues, item.self.loadOffsetTop, item.self.loadOffsetLeft) || isCompletelyInView) && !item.self.loaded) {
+                            loadItemFn(item);
+                        } else if (item.self.isImageLoadedTracker && !item.self.imgError) {
+                            triggerImageInViewportFn({
+                                item,
+                                windowValues,
+                                error: false
+                            });
+                        }
+                        if (isCompletelyInView) {
+                            arrOfLoadedElems.push(item);
+                        }
+                    });
+                    arrOfLoadedElems.forEach(function(item, index) {
+                        // If image loaded remove item from tracking array
+                        removeItemFromArrayFn(arrOfLazyImgElems, item);
+                    });
+                    triggerGlobalDigestFn();
                 }
 
-                function setImageIfInViewportFn(arr) {
-                    var sIFEIA = setImageForElemsInArrayFn(arr);
+                function setImageForElemsInArrayFn() {
                     $window.cancelAnimationFrame(animationFrameTimeoutId);
-                    animationFrameTimeoutId = $window.requestAnimationFrame(sIFEIA);
+                    animationFrameTimeoutId = $window.requestAnimationFrame(setImageIfInViewportFn);
                 }
 
                 window.addEventListener('scroll', function() {
-                    setImageIfInViewportFn(arrOfLazyImgElems, false);
+                    $timeout.cancel(scrollTimeoutId);
+                    scrollTimeoutId = $timeout(function() {
+                        setImageForElemsInArrayFn();
+                    }, 10, false);
                 }, false);
 
-                $rootScope.$on('trigger.ngLazyImg', function() {
-                    setImageIfInViewportFn(arrOfLazyImgElems, false);
+                $rootScope.$on('trigger.ngLazyImg', function(event, timeout = 0) {
+                    $timeout(function() {
+                        setImageForElemsInArrayFn();
+                    }, timeout);
                 });
 
                 return ngLazyLoadImg;
             }
         ]);
-}(window, document);
+}(window, document, angular);
